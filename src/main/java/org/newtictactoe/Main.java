43715257c97e5357fe86f5e91114ef4aed0ef5d2
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2023-02-05 10:12:37 +0400

    Initial commit with Hello World and .gitignore file

---
+++ src/main/java/org/newtictactoe/Main.java
@@ -0,0 +1,13 @@
+package org.newtictactoe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String myStr = new String();
+        String myStr1 = new String();
+        String myStr2 = new String();
+        String myStr3 = new String();
+        String myStr4 = new String();
+        String myStr5 = new String();
+    }
+}
